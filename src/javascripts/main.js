95ca159fd6fa2206ac7ed222b5bb5b20ac2574a4
Joey Petrone <joey.petrone@gmail.com>
2020-03-07 13:14:25 -0600

    Buttons complete

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,4 +1,5 @@
 import studentData from './helpers/data/studentData';
+import houseButtonGroup from './components/houseButtonGroup/houseButtonGroup';
 import navbar from './components/navbar/navbar';
 import studentList from './components/studentList/studentList';
 import '../styles/main.scss';
@@ -6,6 +7,7 @@ import '../styles/main.scss';
 const init = () => {
   const allStudents = studentData.getStudents();
   navbar.loadNavbar();
+  houseButtonGroup.createHouseButtonGroup();
   studentList.createStudentList(allStudents);
 };
 
